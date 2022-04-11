ea37603388706af854cf924b4355e842613a6af3
bk201c <bk201c@outlook.com>
2022-04-11 10:14:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="nav">
+    <ul class="nav-list">
+      <li class="nav-list-item" v-for="item in nav" @click="goto(item)">
+        <img :src="item.icon" :alt="item.name" />
+        <a>{{ item.name }}</a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script setup>
+import nav from "./assets/nav.json";
+const goto = (val) => {
+  const newUrl = val.href.local;
+  window.open(newUrl, "_blank");
+};
+</script>
+<style lang="scss" scoped></style>
