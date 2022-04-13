adec15a5770494fe8fee17f551f4987d8ee68730
bk201c <bk201c@outlook.com>
2022-04-13 17:09:29 +0800

    修复小屏展示问题

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default ({ command, mode }) => {
     // 打包配置
     build: {
       target: "modules",
-      outDir: "dist", //指定输出路径
+      outDir: "dist/navigator/", //指定输出路径
       assetsDir: "assets", // 指定生成静态资源的存放路径
       minify: "terser", // 混淆器，terser构建后文件体积更小
     },
