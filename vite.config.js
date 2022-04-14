63c2389373e5fb3aa6b5e0ef8794ec6be265e5bd
bk201c <bk201c@outlook.com>
2022-04-14 19:37:08 +0800

    增加pwa打包

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default ({ command, mode }) => {
   return defineConfig({
-    base: "/navigator/",
-    plugins: [vue(), vueJsx()],
+    plugins: [vue(), vueJsx(), VitePWA({})],
     assetsInclude: ["**/*.png"],
     // 打包配置
     build: {
       target: "modules",
-      outDir: "dist/navigator/", //指定输出路径
+      outDir: "dist", //指定输出路径
       assetsDir: "assets", // 指定生成静态资源的存放路径
       minify: "terser", // 混淆器，terser构建后文件体积更小
     },
